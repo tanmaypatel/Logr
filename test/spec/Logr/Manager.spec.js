dccca75fdf4e84fd48a5b6a0218b19c30f99977a
Tanmay Patel <tanmay.patel@ge.com>
2014-06-24 18:11:12 +0530

    Added some tests for Manager

--- test/spec/Logr/Manager.spec.js
+++ test/spec/Logr/Manager.spec.js
@@ -10,7 +10,25 @@ define([ 'mocha',
 			it('has static getLogger method', function()
 			{
 				expect(Logr.Manager.getLogger).to.exist;
-			})
+			});
+			
+			it('returns a Logger object on calling it', function()
+			{
+				expect(Logr.Manager.getLogger()).to.be.an.instanceof(Logr.Logger);
+			});
+		});
+		
+		describe('getDefaultConfig method', function()
+		{
+			it('has static getDefaultConfig method', function()
+			{
+				expect(Logr.Manager.getDefaultConfig).to.exist;
+			});
+			
+			it('returns a LoggerConfig object on calling it', function()
+			{
+				expect(Logr.Manager.getDefaultConfig()).to.be.an.instanceof(Logr.LoggerConfig);
+			});
 		});
 	});
  });
