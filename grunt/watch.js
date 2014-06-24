4b1fbe307480cbb6a895dd03a76802ae705a613d
Tanmay Patel <tanmay.patel@ge.com>
2014-06-24 18:02:07 +0530

    Now also watching Gruntfile and grunt/* files to reload grunt build automatically

--- grunt/watch.js
+++ grunt/watch.js
@@ -2,6 +2,13 @@ module.exports = {
 	options: {
 		livereload: true
 	},
+	configFiles: {
+		files: [ 'Gruntfile.js', 'grunt/*.js' ],
+		options: {
+			livereload: false,
+			reload: true
+		}
+	},
 	typescript: {
 		files: [ '<%= srcLocation %>/**/*.ts' ],
 		tasks: [ 'clean:development', 'typescript:development' ]
